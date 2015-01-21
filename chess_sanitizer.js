ba887f8047cc156f5d717dd08c411b75c79003d9
Zachary Aman <zackyuaman@gmail.com>
2015-01-20 22:28:04 -0500

    changed to writeStream to fix async issues

--- chess_sanitizer.js
+++ chess_sanitizer.js
@@ -12,20 +12,47 @@ fs.readdir('./unsanitized', function (err, files) {
 
     //for each file in the unsanitized folder
     for (var i = 0; i < files.length; i++) {
-        console.log(files[i]);
+    //for (var i = 0; i < 1; i++) {
+        var filename = files[i];
 
+        console.log(filename);
 
-        var source = fs.createReadStream('./unsanitized/' + files[i])
+        //clear the file
+        fs.writeFile('./sanitized/' + filename, '', function (err) {
+            if (err) {
+                console.log('error writing to file');
+            }
+        });
+
+        var source = fs.createReadStream('./unsanitized/' + filename)
+        var destination = fs.createWriteStream('./sanitized/' + filename);
         source.pipe(liner)
         liner.on('readable', function () {
             var line
             while (line = liner.read()) {
                 //sanitize the data
-                console.log(line);
+                //console.log(line);
 
-                //write to sanitized folder
+                //probably want information on the file name and the number of moves
+                //want to strip out all of the other metadata
+                var curline = line;
+                if(curline[0] == '['){
+
+                    //do nothing on metadata
+                }
+                else {
+                    console.log(curline);
+                    //add an additional linebreak between games
+                    if(curline[0] == '1' && curline[1] == '.'){
+                        destination.write("\n");
+                    }
+
+                    destination.write(curline + "\n");
+                }
+
             }
         })
+
     }
 });
 
